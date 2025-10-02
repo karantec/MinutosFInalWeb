e712694a44cbc3251db869ecc815811601c0c165
karantec <karanrana3095@gmail.com>
2025-10-02 18:56:56 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { useAppDispatch } from "./Components/hooks/useAppDispatch";
 import CheckoutPage from "./Components/cart/Checkout";
 import PaymentPage from "./Components/cart/Payment";
 import ScrollToTop from "./Components/utils/ScrollTop";
+import CategoriesSub from "./Components/home/Category";
 
 // ✅ Utility to get JWT expiry
 function getTokenExpiry(token) {
@@ -102,6 +103,7 @@ export default function App() {
           path="/subCategory/:categoryName"
           element={<FruitsVegetablesComponent />}
         />
+        <Route path="/subcategory" element={<CategoriesSub />} />
         <Route path="/product/:id" element={<ProductDetailScreen />} />
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/payment" element={<PaymentPage />} />
